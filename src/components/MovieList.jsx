2e0ffac13cb90211406f1be6c1df00fdea782976
aya-othmen7 <ayaothmen7@gmail.com>
2025-07-23 17:51:53 +0100

    link each movie card to its detail page using router

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import MovieCard from './MovieCard';
 import './MovieList.css';
+import { Link } from 'react-router-dom';
 
 const MovieList = ({ movies }) => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} />
+        <Link key={movie.id} to={`/movie/${movie.id}`} className="movie-link">
+          <MovieCard movie={movie} />
+        </Link>
       ))}
     </div>
   );
